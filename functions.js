55ffc6fc64b1bc2d7ff2049b52e197915337a523
J3pe <108808053+erik-thorsell@users.noreply.github.com>
2023-03-25 16:13:45 +0100

    bugfixes + change age automatically

--- functions.js
+++ functions.js
@@ -2,4 +2,22 @@ window.addEventListener('load', () => {
     const loader = document.querySelector('.loader');
     
     loader.classList.add('loader-hidden');
+    document.getElementById('age').innerHTML = calculateAge(12, 10, 2007);
 });
+
+function calculateAge(birthMonth, birthDay, birthYear) {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    var currentMonth = currentDate.getMonth();
+    var currentDay = currentDate.getDate(); 
+    var calculatedAge = currentYear - birthYear;
+
+    if (currentMonth < birthMonth - 1) {
+        calculatedAge--;
+    }
+    if (birthMonth - 1 == currentMonth && currentDay < birthDay) {
+        calculatedAge--;
+    }
+    return calculatedAge;
+}
+
